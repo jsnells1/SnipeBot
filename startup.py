2b9c5746bc2951b43bae2f70fbdfddef5bbb3547
jsnellings1 <jsnellings1@gulls.salisbury.edu>
2019-04-15 20:50:53 -0400

    Added launch configs for dev or live environments

--- startup.py
+++ startup.py
@@ -1,13 +1,20 @@
+import argparse
+
+import discord
 from discord.ext.commands import Bot, Context, has_permissions, MissingPermissions
 
 from sniping.snipe import Snipes
 from admin.admin import AdminCommands
 from soapbox.soapbox import Soapbox
 
+from data import code
+from data.code import Environment
+
 TOKEN = 'NTY2MDU0MTYzNDk5NDUwMzk5.XK_aKg.-dGUgE3J1JgnJaVrnEJS8gFQDKw'
 BOT_PREFIX = "!"
 
-bot = Bot(command_prefix=BOT_PREFIX, case_insensitive=True)
+bot = Bot(command_prefix=BOT_PREFIX, case_insensitive=True,
+          activity=discord.Activity(type=discord.ActivityType.watching, name='Nathan\'s snap story'))
 
 
 @bot.command(name='q2', hidden=True)
@@ -26,7 +33,19 @@ async def kill_error(error, ctx: Context):
 
 @bot.event
 async def on_ready():
-    print('Ready')
+    print('Ready. Database: ' + code.DATABASE)
+
+# Setup
+parser = argparse.ArgumentParser()
+parser.add_argument('-env')
+args = parser.parse_args()
+
+if args.env is not None:
+    if args.env == 'dev':
+        code.switchDatabase(Environment.dev)
+    elif args.env == 'live':
+        code.switchDatabase(Environment.live)
+
 
 bot.add_cog(Soapbox(bot))
 bot.add_cog(Snipes(bot))
