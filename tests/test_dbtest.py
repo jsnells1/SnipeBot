fc2f1e820fdcc9d321a674f6d9bf76cb31cabbd7
jsnellings1 <jsnellings1@gulls.salisbury.edu>
2019-04-30 19:41:55 -0400

    Updated tests for travis testing

--- tests/test_dbtest.py
+++ tests/test_dbtest.py
@@ -1,7 +1,25 @@
 import pytest
-# from data import code as Database
-# from data.code import Environment
+
+from data import code as Database
+from data.code import Environment
+
+
+def setup_module(module):
+    Database.switchDatabase(Environment.dev)
+
+
+def setup_function(function):
+    if Database.DATABASE != Database.DEV_DATABASE:
+        pytest.fail('Database not set to dev')
 
 
 def test_example():
+    assert 1 == 1
+
+
+def test_example2():
+    assert 1 == 1
+
+
+def test_example3():
     assert 1 == 1
